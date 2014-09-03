199f1216fe26cb4538555c6fae05572c7ac2652b
Bill Walker <bwalker@mozilla.com>
2014-09-03 16:58:48 -0700

    make this into a birding by ear quiz, as a sample useful application

--- js/app.js
+++ js/app.js
@@ -1,25 +1,24 @@
 // app.js
 
-var scienceNames = [];
+var sightings = [];
 var sounds = [];
 
 function getLocation() {
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(
 			function success(position) {
-				console.log('success');
-				console.log(position);
 				$('#location').append('<div>' + position.coords.latitude + ',' + position.coords.longitude + '</div>');
 				getRecentNearbySightings(position.coords.latitude, position.coords.longitude);
 			},
 			function error() {
 				console.log('error');
+				getRecentNearbySightings(37, -122);
 			},
 			{
 			});
 	} else {
-		$('#location').append('<div>23,34</div>')
-		getRecentNearbySightings(23, 34);
+		$('#location').append('<div>37, -122</div>')
+		getRecentNearbySightings(37, -122);
 	}
 }
 
@@ -31,42 +30,51 @@ function getSounds(inID, latinName) {
 		url: urlString,
 		dataType: 'json',
 		success: function(data) {
-			console.log('got sound data');
-			console.log(data);
-			sounds = data;
-
-			console.log(data.recordings[0].file);
-			$('#' + inID).append('<audio src="' + data.recordings[0].file + '" type="audio/mpeg" controls loop></audio>')
+			sounds[inID] = data.recordings[0].file;
+			if (data.recordings) {
+				$('#quiz').append('<audio src="' + data.recordings[0].file + '" type="audio/mpeg" autoplay controls loop></audio>')
+			} else {
+				$('#quiz').append('no recordings found');
+			}
 		},
 		error: function(xhr, status, error) {
 			console.log('xeno canto fail');
-			console.log(xhr.status);
-			console.log(xhr.statusText);
+			$('#quiz').append('no recordings found');
 		}
 	});
 }
 
 function getRecentNearbySightings(inLatitude, inLongitude) {
+	$('#location').append('<img src="http://maps.googleapis.com/maps/api/staticmap?markers=color:blue|size:small|' + inLatitude + ',' + inLongitude + '&size=100x100&zoom=5&sensor=false" />');
+
 	var queryParams = { lat: inLatitude, lng: inLongitude, fmt: 'json' };
 	var urlString = 'http://ebird.org/ws1.1/data/obs/geo/recent?' + $.param(queryParams);
 	console.log(urlString);
 
 	$.getJSON(urlString, function(data) {
 		console.log('got data');
-		console.log(data);
+		$('#location').append('<div>' + data.length + ' sightings</div>')
 		for (var index in data) {
-			$('#scienceNames').append('<div id="' + index + '">' + data[index].comName + '</div>');
-			scienceNames.push(data[index].sciName);
-		}
-
-		for (index in data) {
-			getSounds(index, scienceNames[index]);
+			sightings.push(data[index]);
 		}
 	});
 }
 
 $(document).ready(function(){ 
 	console.log('hello, world');
+
+	$('#choose').click(function() {
+		var chosen = Math.floor(Math.random() * sightings.length);
+		$('#quiz').empty();
+		$('#quiz').append('<h1>#' + chosen + '</h1>');
+		$('#quiz').append('<div id="commonname" style="display: none">' + sightings[chosen].comName + '</div>');
+		getSounds(chosen, sightings[chosen].sciName);
+	});
+
+	$('#answer').click(function() {
+		$('#commonname').show();
+	});
+
 	getLocation();
 });
 
